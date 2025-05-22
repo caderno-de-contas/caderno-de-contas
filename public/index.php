c65c1437b90af79d73ee6d82270d26b902e18925
Gilglécio Oliveira <gilglecio_765@hotmail.com>
2025-05-22 15:04:16 -0300

    add enviroment, twig and sqlite

--- public/index.php
+++ public/index.php
@@ -1,1 +1,21 @@
-<?php echo phpinfo();
+<?php
+
+require_once 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$db = new SQLite3('db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+$db->enableExceptions(true);
+
+$loader = new \Twig\Loader\FilesystemLoader('src/templates');
+
+$twig_options = [];
+
+if ($_ENV['APP_ENV'] != 'local') {
+    $twig_options['cache'] = '.twig-cache';
+}
+
+$twig = new \Twig\Environment($loader, $twig_options);
+
+echo $twig->render('index.html.twig', []);
