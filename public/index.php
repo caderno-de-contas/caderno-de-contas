2003aee08ef734d8420302ee9a54c4f2f51064d3
Gilglécio Oliveira <gilglecio_765@hotmail.com>
2025-05-23 07:52:50 -0300

    add slim 4

--- public/index.php
+++ public/index.php
@@ -2,25 +2,74 @@
 
 require_once 'vendor/autoload.php';
 
+use DI\Container;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 define('ENV_PRODUCTION', $_ENV['APP_ENV'] == 'production');
 
-$builder = new \DI\ContainerBuilder();
-if (ENV_PRODUCTION) {
-    $builder->enableCompilation('.tmp');
-    $builder->writeProxiesToFile(true, '.tmp/proxies');
-}
-$container = $builder->build();
+$container = new Container();
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$twig = Twig::create('src/templates', ['cache' => ENV_PRODUCTION]);
+
+$app->add(TwigMiddleware::create($app, $twig));
 
 $db = new SQLite3('db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 $db->enableExceptions(true);
 
-$loader = new \Twig\Loader\FilesystemLoader('src/templates');
-$twig_options = [];
-if (ENV_PRODUCTION) {
-    $twig_options['cache'] = '.twig-cache';
-}
-$twig = new \Twig\Environment($loader, $twig_options);
-echo $twig->render('index.html.twig', []);
+$app->addRoutingMiddleware();
+
+$logger = new Logger('error');
+$logger->pushHandler(new RotatingFileHandler('error.log'));
+
+$customErrorHandler = function (
+    ServerRequestInterface $request,
+    Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails
+) use ($app, $logger) {
+    if ($logger) {
+        $logger->error($exception->getMessage());
+    }
+
+    $payload = [
+        'error' => $exception->getMessage(),
+    ];
+
+    if (!ENV_PRODUCTION) {
+        $payload['exception'] = [
+            'in' => $exception->getFile().':'.$exception->getLine(),
+            'trace' => $exception->getTrace(),
+        ];
+    }
+
+    $response = $app->getResponseFactory()->createResponse();
+    $response->getBody()->write(
+        json_encode($payload, JSON_UNESCAPED_UNICODE)
+    );
+
+    return $response->withHeader('Content-Type', 'application/json');
+};
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true, $logger);
+$errorMiddleware->setDefaultErrorHandler($customErrorHandler);
+
+$app->get('/', function ($request, $response) {
+    $view = Twig::fromRequest($request);
+    
+    return $view->render($response, 'index.html.twig', [
+        'name' => 'John',
+    ]);
+});
+
+$app->run();
